b6b52dedf8df1ad50fa5799644693993e7bd0545
Douglas Fils <drfils@gmail.com>
2020-03-14 19:23:53 -0500

    Add in the Blank node sugar funcations

--- Go/netcdf-golang/internal/nc2rdf/triplegen.go
+++ Go/netcdf-golang/internal/nc2rdf/triplegen.go
@@ -87,3 +87,119 @@ func IIITriple(s, p, o, c string) (string, error) {
 	}
 	return buf.String(), err
 }
+
+// IIBTriple builds a IRI, IRI, Blank triple
+func IIBTriple(s, p, o, c string) (string, error) {
+	buf := bytes.NewBufferString("")
+
+	newctx, err := rdf.NewIRI(c) // this should be  c
+	if err != nil {
+		return buf.String(), err
+	}
+	ctx := rdf.Context(newctx)
+
+	sub, err := rdf.NewIRI(s)
+	if err != nil {
+		log.Println("Error building subject IRI for tika triple")
+		return buf.String(), err
+	}
+	pred, err := rdf.NewIRI(p)
+	if err != nil {
+		log.Println("Error building predicate IRI for tika triple")
+		return buf.String(), err
+	}
+	obj, err := rdf.NewBlank(o)
+	if err != nil {
+		log.Println("Error building object literal for tika triple")
+		return buf.String(), err
+	}
+
+	t := rdf.Triple{Subj: sub, Pred: pred, Obj: obj}
+	q := rdf.Quad{Triple: t, Ctx: ctx}
+
+	qs := q.Serialize(rdf.NQuads)
+	if s != "" && p != "" && o != "" {
+		fmt.Fprintf(buf, "%s", qs)
+	}
+	return buf.String(), err
+}
+
+// BILTriple builds a Blank, IRI, Literal triple
+func BILTriple(s, p, o, c string) (string, error) {
+	buf := bytes.NewBufferString("")
+
+	newctx, err := rdf.NewIRI(c) // this should be  c
+	if err != nil {
+		return buf.String(), err
+	}
+	ctx := rdf.Context(newctx)
+
+	sub, err := rdf.NewBlank(s)
+	if err != nil {
+		log.Println("Error building subject IRI")
+		return buf.String(), err
+	}
+	pred, err := rdf.NewIRI(p)
+	if err != nil {
+		log.Println("Error building predicate IRI")
+		return buf.String(), err
+	}
+
+	// type to xsd:double for float64
+	dt, err := rdf.NewIRI("http://www.w3.org/2001/XMLSchema#double")
+	if err != nil {
+		log.Println("Error building predicate IRI for xsd type")
+		return buf.String(), err
+	}
+
+	obj := rdf.NewTypedLiteral(o, dt) // why do typed litterials not return error?
+	// if err != nil {
+	// 	log.Println("Error building object literal")
+	// 	return buf.String(), err
+	// }
+
+	t := rdf.Triple{Subj: sub, Pred: pred, Obj: obj}
+	q := rdf.Quad{Triple: t, Ctx: ctx}
+
+	qs := q.Serialize(rdf.NQuads)
+	if s != "" && p != "" && o != "" {
+		fmt.Fprintf(buf, "%s", qs)
+	}
+	return buf.String(), err
+}
+
+// BIITriple builds a Blank, IRI, IRI triple
+func BIITriple(s, p, o, c string) (string, error) {
+	buf := bytes.NewBufferString("")
+
+	newctx, err := rdf.NewIRI(c) // this should be  c
+	if err != nil {
+		return buf.String(), err
+	}
+	ctx := rdf.Context(newctx)
+
+	sub, err := rdf.NewBlank(s)
+	if err != nil {
+		log.Println("Error building subject IRI for tika triple")
+		return buf.String(), err
+	}
+	pred, err := rdf.NewIRI(p)
+	if err != nil {
+		log.Println("Error building predicate IRI for tika triple")
+		return buf.String(), err
+	}
+	obj, err := rdf.NewIRI(o)
+	if err != nil {
+		log.Println("Error building object literal for tika triple")
+		return buf.String(), err
+	}
+
+	t := rdf.Triple{Subj: sub, Pred: pred, Obj: obj}
+	q := rdf.Quad{Triple: t, Ctx: ctx}
+
+	qs := q.Serialize(rdf.NQuads)
+	if s != "" && p != "" && o != "" {
+		fmt.Fprintf(buf, "%s", qs)
+	}
+	return buf.String(), err
+}
